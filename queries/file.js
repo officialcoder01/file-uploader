8c82e307d9d558854834c779f3fcda5e54a72da1
Zion Musa <musazion986@gmail.com>
2025-12-16 22:20:55 +0100

    Used one prisma instant across all files instead of recreating every time

--- queries/file.js
+++ queries/file.js
@@ -1,11 +1,4 @@
-const { PrismaClient } = require('@prisma/client');
-const { PrismaPg } = require('@prisma/adapter-pg');
-require('dotenv').config();
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
-const prisma = new PrismaClient({ adapter });
+const prisma = require('../lib/prisma');
 
 const createFile = async (fileData) => {
     return await prisma.file.create({
