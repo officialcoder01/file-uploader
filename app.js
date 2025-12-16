8897c817f2c0fd707eb9c0fe8dc94cf4a540cf30
Zion Musa <musazion986@gmail.com>
2025-12-16 01:25:34 +0100

    App entry point

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const session = require('express-session');
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
+const { PrismaClient } = require('@prisma/client');
+const { PrismaPg } = require('@prisma/adapter-pg');
+const passport = require('passport');
+const path = require('node:path');
+const flash = require('connect-flash');
+const authRouter = require('./routes/authRouter');
+const fileRouter = require('./routes/fileRouter');
+const folderRouter = require('./routes/folderRouter');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.set('view cache', false);
+
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+const prisma = new PrismaClient({ adapter });
+
+app.use(
+  session({
+    cookie: {
+     maxAge: 7 * 24 * 60 * 60 * 1000 // ms
+    },
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaSessionStore(
+      prisma,
+      {
+        checkPeriod: 2 * 60 * 1000,  //ms
+        dbRecordIdIsSessionId: true,
+        dbRecordIdFunction: undefined,
+      }
+    )
+  })
+);
+app.use(flash());
+
+// expose flash messages to all views
+app.use((req, res, next) => {
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', authRouter);
+app.use('/files', fileRouter);
+app.use('/folders', folderRouter);
+
+// Start the server
+const PORT = process.env.PORT || 3000
+app.listen(PORT, (error) => {
+    if (error) {
+        console.error('Error starting the server:', error);
+    } else {
+        console.log(`Server is running on port ${PORT}`);
+    }
+});
